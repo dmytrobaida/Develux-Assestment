5d634dc8405666b398adb23561f6c5846ad9c5b2
dmytrobaida <dmytrobaida@outlook.com>
2024-04-10 15:21:01 +0300

    improve readability

--- src/create-pull-request.ts
+++ src/create-pull-request.ts
@@ -12,23 +12,29 @@ async function main() {
     // Get all command line arguments
     const { packageWithVersion, pathToPackageJson, repo, branch, title } =
       await getInputArguments();
+
     // Create api instance
     const api = new BitBucketApi(repo);
+
     // Read original package.json file
     console.log("Reading original package.json...");
     const packageJsonStr = await api.readFile(srcBranch, pathToPackageJson);
+
     // Update package json with new package version
     console.log("Updating package.json with new package version...");
     const fileContent = Buffer.from(
       processPackageJson(packageJsonStr, packageWithVersion),
       "utf-8"
     );
+
     // Create new branch with updated package json
     console.log("Creating new branch and commit with new changes...");
     await api.createCommitFromFile(branch, fileContent, destFile);
+
     // Create pull request
     console.log("Creating pull request...");
     await api.createPullRequest(branch, title);
+
     // Print result
     console.log("Success!");
   } catch (err) {
